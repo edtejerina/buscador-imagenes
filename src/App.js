4318037b379a71daee67e093d4b93e07e4c019ca
Edgar Tejerina <edtejerina.work@gmail.com>
2020-04-17 00:24:50 -0300

    My first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Buscador from './components/Buscador'
+import Resultado from './components/Resultado'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  state = {
+      termino : '',
+      imagenes: [],
+      pagina: ''
+  }
+  
+  scroll = () => {
+    const elemento = document.querySelector('.jumbotron');
+    elemento.scrollIntoView('smooth', 'start');
+  }
+  paginaAnterior = () => {
+    //lee el state de la pagina actual
+    let pagina = this.state.pagina;
+    //resta uno a la pagina actual
+    if(pagina === 1) return null;
+    pagina--;
+    //agrega el cambio al state
+    this.setState({
+      pagina
+    }, () => {
+      this.consultarApi();
+      this.scroll();
+    });
+  }
+  paginaSiguiente = () => {
+    //lee el state de la pagina actual
+    let pagina = this.state.pagina;
+    //suma uno a la pagina actual
+    pagina++;
+    //agrega el cambio al state
+    this.setState({
+      pagina
+    }, () => {
+      this.consultarApi();
+      this.scroll();
+    });
+  }
+  //Peticion fetch a la api de pixabay
+  consultarApi = () => {
+    const termino  = this.state.termino;
+    const pagina = this.state.pagina;
+    const url = `https://pixabay.com/api/?key=14153040-4f429875226383235ab1b9184&q=${termino}&per_page=30&page=${pagina}`;
+    fetch(url)
+    .then(response => response.json())
+    .then(result => this.setState({imagenes: result.hits}))
+  }
+  datosBusqueda = (termino) => {
+    this.setState({
+      termino
+    }, () => {
+      this.consultarApi();
+    })
+  }
+  render(){
+    return (
+      <div className="App container">
+        <div className="jumbotron">
+          <p className="lead text-center">Buscador de Imágenes</p>
+          <Buscador 
+            datosBusqueda = {this.datosBusqueda}
+          />
+        </div>
+        <div className="row justify-content-center">
+          <Resultado 
+            imagenes = {this.state.imagenes}
+            paginaAnterior = {this.paginaAnterior}
+            paginaSiguiente = {this.paginaSiguiente}
+          />
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
